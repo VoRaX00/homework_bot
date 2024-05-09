0bb66bcf45bdcaf6a841fb3d3ac71e2b3a42e57f
Kerzhakov Nikita Alexeevich <nikitakerzhik@gmail.com>
2024-05-10 01:51:18 +1000

    почему-то не работает callback...

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module main.go
 go 1.22.1
 
 require (
+	github.com/crocone/tg-bot v0.0.0-20230406135341-8d3e6a822eaf // indirect
 	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible // indirect
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,3 +1,122 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"log"
+// 	"os"
+
+// 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+// 	"github.com/joho/godotenv"
+// )
+
+// var bot *tgbotapi.BotAPI
+
+// func startMenu() tgbotapi.InlineKeyboardMarkup {
+// 	btnHi := tgbotapi.NewInlineKeyboardButtonData("Привет", "hi") // tgbotapi.NewKeyboardButton("Привет")
+// 	btnBye := tgbotapi.NewInlineKeyboardButtonData("Пока", "bye") // tgbotapi.NewKeyboardButton("Пока")
+
+// 	row := tgbotapi.NewInlineKeyboardRow(btnHi, btnBye)
+// 	// Создаем строки с кнопками
+// 	// row1 := []tgbotapi.InlineKeyboardButton{btnHi}
+// 	// row2 := []tgbotapi.InlineKeyboardButton{btnBye}
+
+// 	// Создаем клавиатуру из кнопок и строк
+// 	keyboard := tgbotapi.NewInlineKeyboardMarkup(row)
+// 	return keyboard
+// }
+
+// func commands(update tgbotapi.Update) { //функция которая будет реагировать на команды в чате
+// 	command := update.Message.Command()
+
+// 	switch command {
+// 	case "start":
+// 		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Я бот и я помогу искать дз проще!!!")
+// 		msg.ReplyMarkup = startMenu()
+// 		sendMessage(msg)
+// 	}
+// }
+
+// func callbacks(update tgbotapi.Update) {
+// 	callbackData := update.CallbackQuery.Data
+// 	chatID := update.CallbackQuery.Message.Chat.ID
+
+// 	switch callbackData {
+// 	case "hi":
+// 		text := fmt.Sprintf("Привет %v", update.CallbackQuery.Message.Chat.FirstName)
+// 		msg := tgbotapi.NewMessage(chatID, text)
+// 		bot.Send(msg)
+
+// 	case "bye":
+// 		text := fmt.Sprintf("Пока %v", update.CallbackQuery.Message.Chat.FirstName)
+// 		msg := tgbotapi.NewMessage(chatID, text)
+// 		bot.Send(msg)
+// 	}
+// }
+
+// func sendMessage(msg tgbotapi.Chattable) {
+// 	if _, err := bot.Send(msg); err != nil {
+// 		fmt.Println(err.Error())
+// 	}
+// }
+
+// func main() {
+// 	err := godotenv.Load(".env")
+// 	if err != nil {
+// 		log.Fatal(".env not loaded")
+// 	}
+
+// 	bot, err = tgbotapi.NewBotAPI(os.Getenv("TG_API_TOKEN"))
+// 	if err != nil {
+// 		fmt.Println(err.Error())
+// 		return
+// 	}
+
+// 	bot.Debug = true
+
+// 	//log.Printf("Authorized on account %s", bot.Self.UserName)
+
+// 	u := tgbotapi.NewUpdate(0)
+// 	u.Timeout = 10
+
+// 	updates, err := bot.GetUpdatesChan(u)
+
+// 	if err != nil {
+// 		fmt.Println(err.Error())
+// 		return
+// 	}
+// 	// Loop through each update.
+// 	for update := range updates {
+// 		// Check if we've gotten a message update.
+
+// 		if update.Message.IsCommand() {
+// 			commands(update)
+// 		} else if update.CallbackQuery != nil {
+// 			callbacks(update)
+
+// 			//callback := tgbotapi.NewCallback(update.CallbackQuery.ID, update.CallbackQuery.Data)
+
+// 			// if _, err := bot.Request(callback); err != nil {
+// 			// 	panic(err)
+// 			// }
+
+// 			// msg := tgbotapi.NewMessage(update.CallbackQuery.Message.Chat.ID, update.CallbackQuery.Data)
+// 			// if _, err := bot.Send(msg); err != nil {
+// 			// 	panic(err)
+// 			// }
+// 		} else {
+// 			println("simple message")
+// 		}
+
+// 		if update.Message == nil { // Ignore any non-Message updates
+// 			continue
+// 		}
+// 		// } else { //если наше сообщение не команда, то мы делаем эхо
+// 		// 	msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+// 		// 	bot.Send(msg)
+// 		// }
+// 	}
+// }
+
 package main
 
 import (
@@ -5,26 +124,35 @@ import (
 	"log"
 	"os"
 
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	tgbotapi "github.com/crocone/tg-bot"
 	"github.com/joho/godotenv"
 )
 
-var startMenu = tgbotapi.NewInlineKeyboardMarkup(
-	tgbotapi.NewInlineKeyboardRow(
-		tgbotapi.NewInlineKeyboardButtonData("Что ты умеешь?", "я умею..."),
-	),
-)
+var bot *tgbotapi.BotAPI
 
-func callback(update tgbotapi.Update) {
-	fmt.Print("hello")
+type button struct {
+	name string
+	data string
 }
 
-func commands(update tgbotapi.Update) {
-	command := update.Message.Command()
-	switch command {
-	case "start":
-		//msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Выберите действие")
+func startMenu() tgbotapi.InlineKeyboardMarkup {
+	states := []button{
+		{
+			name: "Привет",
+			data: "hi",
+		},
+		{
+			name: "Пока",
+			data: "buy",
+		},
 	}
+
+	buttons := make([][]tgbotapi.InlineKeyboardButton, len(states))
+	for index, state := range states {
+		buttons[index] = tgbotapi.NewInlineKeyboardRow(tgbotapi.NewInlineKeyboardButtonData(state.name, state.data))
+	}
+
+	return tgbotapi.NewInlineKeyboardMarkup(buttons...)
 }
 
 func main() {
@@ -33,39 +161,60 @@ func main() {
 		log.Fatal(".env not loaded")
 	}
 
-	bot, err := tgbotapi.NewBotAPI(os.Getenv("TG_API_TOKEN"))
-
+	bot, err = tgbotapi.NewBotAPI(os.Getenv("TG_API_TOKEN"))
 	if err != nil {
-		log.Panic(err)
+		log.Fatalf("Failed to initialize Telegram bot API: %v", err)
 	}
 
-	bot.Debug = true
-
-	log.Printf("Authorized on account %s", bot.Self.UserName)
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
-
-	updates, err := bot.GetUpdatesChan(u)
+	updates := bot.GetUpdatesChan(u)
 	if err != nil {
-		log.Panic(err)
+		log.Fatalf("Failed to start listening for updates %v", err)
 	}
 
 	for update := range updates {
 		if update.CallbackQuery != nil {
-			callback(update)
+			callbacks(update)
 		} else if update.Message.IsCommand() {
 			commands(update)
 		} else {
-			if update.Message == nil { // игнорируем любое не сообщение
-				continue
-			}
-
-			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-			msg.ReplyToMessageID = update.Message.MessageID
-
-			bot.Send(msg)
+			// simply message
 		}
+	}
+}
+
+func callbacks(update tgbotapi.Update) {
+	data := update.CallbackQuery.Data
+	chatId := update.CallbackQuery.From.ID
+	firstName := update.CallbackQuery.From.FirstName
+	lastName := update.CallbackQuery.From.LastName
+	var text string
+	switch data {
+	case "hi":
+		text = fmt.Sprintf("Привет %v %v", firstName, lastName)
+	case "buy":
+		text = fmt.Sprintf("Пока %v %v", firstName, lastName)
+	default:
+		text = "Неизвестная команда"
+	}
+	msg := tgbotapi.NewMessage(chatId, text)
+	sendMessage(msg)
+}
+
+func commands(update tgbotapi.Update) {
+	command := update.Message.Command()
+	switch command {
+	case "start":
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Выберите действие")
+		msg.ReplyMarkup = startMenu()
+		msg.ParseMode = "Markdown"
+		sendMessage(msg)
+	}
+}
+
+func sendMessage(msg tgbotapi.Chattable) {
+	if _, err := bot.Send(msg); err != nil {
+		log.Panicf("Send message error: %v", err)
 	}
 }
