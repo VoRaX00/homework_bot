aa4802605db4a443da22d349501fd930e7b82e2d
Kerzhakov Nikita Alexeevich <nikitakerzhik@gmail.com>
2024-09-04 22:19:41 +0500

    Added migrations on github

---
+++ storage/migrations/20240818202859_create_sources.sql
@@ -0,0 +1,32 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE homework (
+    id SERIAL NOT NULL UNIQUE ,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(1024),
+    images TEXT[],
+    create_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    deadline TIMESTAMP NOT NULL DEFAULT NOW(),
+    update_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE tags (
+    id SERIAL NOT NULL UNIQUE,
+    name VARCHAR(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE homeworks_tags (
+    id SERIAL PRIMARY KEY UNIQUE,
+    homework_id INT REFERENCES homework (id) ON DELETE CASCADE NOT NULL,
+    tag_id INT REFERENCES tags (id) ON DELETE CASCADE NOT NULL
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE homeworks_tags;
+
+DROP TABLE homework;
+
+DROP TABLE tags;
+-- +goose StatementEnd
