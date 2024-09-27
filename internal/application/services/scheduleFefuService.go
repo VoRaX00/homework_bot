f7c2b4971932d1b788a6ffcd5d50995c1bc95555
nikita <nikitakerzhik@gmail.com>
2024-09-27 16:09:04 +1000

    added Parser and edit service

--- internal/application/services/scheduleFefuService.go
+++ internal/application/services/scheduleFefuService.go
@@ -1,9 +1,10 @@
 package services
 
 import (
+	"fmt"
+	"github.com/sirupsen/logrus"
 	"homework_bot/internal/domain"
 	"homework_bot/pkg/scheduleParser"
-	"os"
 	"time"
 )
 
@@ -17,8 +18,14 @@ func NewScheduleFefuService() *ScheduleFefuService {
 	}
 }
 
+func generateLink(typeSchedule string, firstDate, secondDate time.Time) string {
+	link := fmt.Sprintf("https://univer.dvfu.ru/schedule/get?type=%s&start=%s", typeSchedule, firstDate.Format("2006-01-02"))
+	link += "T14%3A00%3A00.000Z&" + fmt.Sprintf("end=%s", secondDate.Format("2006-01-02")) + "T14%3A00%3A00.000Z&groups%5B%5D=5560&ppsId=&facilityId=0"
+	return link
+}
+
 func (s *ScheduleFefuService) GetOnDate(date time.Time) domain.Schedule {
-	link := os.Getenv("SCHEDULE_LINK")
+	link := generateLink("agendaDay", date.Add(-24*time.Hour), date)
 	res, err := s.parser.ParseSchedule(link)
 	if err != nil {
 		return domain.Schedule{}
@@ -26,14 +33,30 @@ func (s *ScheduleFefuService) GetOnDate(date time.Time) domain.Schedule {
 	return res
 }
 
+func getDatesForWeek() (time.Time, time.Time) {
+	date := time.Now()
+	dayOfWeek := int(date.Weekday())
+	lastSunday := date.AddDate(0, 0, -dayOfWeek-7)
+	saturday := date.AddDate(0, 0, 6-dayOfWeek)
+	return lastSunday, saturday
+}
+
 func (s *ScheduleFefuService) GetOnWeek() domain.Schedule {
-	return domain.Schedule{}
+	lastSunday, saturday := getDatesForWeek()
+	link := generateLink("agendaDay", lastSunday, saturday)
+
+	res, err := s.parser.ParseSchedule(link)
+	if err != nil {
+		logrus.Errorf("Error in parse schedule, %v", err)
+		return domain.Schedule{}
+	}
+	return res
 }
 
 func (s *ScheduleFefuService) GetOnToday() domain.Schedule {
-	return domain.Schedule{}
+	return s.GetOnDate(time.Now())
 }
 
 func (s *ScheduleFefuService) GetOnTomorrow() domain.Schedule {
-	return domain.Schedule{}
+	return s.GetOnDate(time.Now().AddDate(0, 0, 1))
 }
